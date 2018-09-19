ac69cc80b44f2d5947384318f181a7b14c6cccbc
unknown <ereke_enu@mail.ru>
2018-09-19 18:44:20 +0600

    start

---
+++ src/main/java/kz/smartdev/adminka/AdminkaApplication.java
@@ -0,0 +1,12 @@
+package kz.smartdev.adminka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminkaApplication.class, args);
+	}
+}
